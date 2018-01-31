1ee4429d3d577cc88f9f8d26fa0863276c988b92
George Crisan <georgerdp@gmail.com>
2018-01-31 14:55:14 +0000

    go last time

--- assets/main.js
+++ assets/main.js
@@ -3,13 +3,13 @@ function renderGallery(){
       
 
       _.forEach($('.gallery a'),function(element,key){
-         $(element).attr('href','/assets/images/omie/p' + key + '.jpg');    
+         $(element).attr('href','assets/images/omie/p' + key + '.jpg');    
         console.log(element);
             console.log(key);
       });
 
       _.forEach($('.gallery a img'),function(element,key){
-            $(element).attr('src','/assets/images/treisutezece/p' + key + '.jpg');     
+            $(element).attr('src','assets/images/treisutezece/p' + key + '.jpg');     
             
       });
 
